0bf3f8aa936003bed2a61ebca0c75dec23480e90
Patrick Knowles <pgknowles1@gmail.com>
2026-09-06 15:38:46 -0600

    article update

--- the_alternative_f1/articles/Season5/Race_Week/australia_race_week.py
+++ the_alternative_f1/articles/Season5/Race_Week/australia_race_week.py
@@ -3,7 +3,7 @@ from the_alternative_f1.articles.components import zoomable_image, image_carouse
 
 article = {
     "title": "Race Week: Australia",
-    "blurb": "Upside-down and ready to roar. The league eyes Australia for their first race of the saeson. Will the preseason rankings hold on or will a new pecking order begin to shine through.",
+    "blurb": "Upside-down and ready to roar. The league eyes Australia for their first race of the saeson. Will the preseason rankings hold on or will a new pecking order begin to shine through?",
     "content": [
         "This season, The Intern and I have clear role definiton on who is authoring what. \
         You will find hard hitting analysis here about the upcoming race as well as power rankings \
@@ -14,16 +14,16 @@ article = {
         "First and foremost, the power rankings have been updated to reflect results from throughout \
             the preseason. Red Bull's new duo has proven to be powerful, even as a solo show in two of \
                 the three races. Many pundits early on, pushed McLaren to the very front of the rankings \
-                    until both Cadiillac and Red Bull showed their form. Between the three teams, there \
-                        is nearly a standstill on who is likely to take home the crown. While Red bull, \
-                            McLaren, and Cadillac are the front runners, they are chased closely by \
+                    until both Cadillac and Red Bull showed their form. Between the three teams, there \
+                        is nearly a standstill on who is likely to take home the crown. While Red Bull, \
+                            McLaren, and Cadillac are the early front runners, they are chased closely by \
                                 Ferrari and Haas who sit in a two way battle to prove if they are best \
                                     of the rest or a top three podium team.",
         "Mercedes might be the most interesting and difficult to gauge right now. Insider sources say \
-            Randy has been seen completing Testing of Previous cars in Melbourne and showing times \
-                near the front of the field. The question is, will Jairo live up to his promise of \
+            Randy has been seen completing Testing of Previous Cars in Melbourne and showing times \
+                near the front of the field. The most important question is, will Jairo live up to his promise of \
                     murdering Joshua?",
-        "Finally, Audi and Williams both simply live ina  shroud of mystery. Either could produce \
+        "Finally, Audi and Williams both simply live in a shroud of mystery. Either could produce \
             electric rookie seasons and with Audi's vet finding form with new equipment, the sky is \
                 the limit for either outfit.",
         rx.box(
@@ -49,7 +49,7 @@ article = {
                             and tire management will prove to be true or just another ghost tale.",
                 "Melbourne provides more overtaking places than one might notice at first glance. The obvious \
                     locations like Turn 1 and Turn 9 provide excellent places for battery micro-managers to power \
-                        past their opponents. Along with Turn 3, Turn 11, and Turn 13, drivers will have all the \
+                        pass their opponents. Along with Turn 3, Turn 11, and Turn 13, drivers will have all the \
                             options to show their motorsport prowess and creativity.",
                 color="#E0E0E0",
                 font_size="md",
@@ -61,7 +61,7 @@ article = {
         "With anticipation floating through the air \
             for what is lined up to be a historic season, only a few days\
                 and the driver's nervous jitters remain between now and the first official race day \
-                    of Season 5. May the odds be ever in your favor."
+                    of The Alternative F1's Season 5. May the odds be ever in your favor."
     ],
     "image": "/thealternativef1-cloudflare/Season5/Race_Week/Australia_Cover.jpeg",
     "author": "Patrick",
